d7959f5575276fa1ea6296db3b64901330636ce2
shoaib attari <shoaibattari83@gmail.com>
2025-11-05 13:48:52 +0500

    add areaCoordinator role to userRole enum in user model

--- models/userModel.js
+++ models/userModel.js
@@ -18,6 +18,7 @@ const userSchema = new Schema(
         "superAdmin",
         "admin",
         "eventCoordinator",
+        "areaCoordinator",
         "volunteer",
         "student",
         "participant",
